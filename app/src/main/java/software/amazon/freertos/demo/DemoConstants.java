6cf2a5c645b418bf08bd55cd0256f2d75c8961b0
brindha2822 <65030992+brindha2822@users.noreply.github.com>
2022-03-21 14:42:54 +0530

    Update DemoConstants.java

--- app/src/main/java/software/amazon/freertos/demo/DemoConstants.java
+++ app/src/main/java/software/amazon/freertos/demo/DemoConstants.java
@@ -4,9 +4,9 @@ public class DemoConstants {
     /*
      * Replace with your AWS IoT policy name.
      */
-    final static String AWS_IOT_POLICY_NAME = "Your AWS IoT policy name";
+    final static String AWS_IOT_POLICY_NAME = "Brindha_iot_policy";
     /*
      * Replace with your AWS IoT region, eg: us-west-2.
      */
-    final static String AWS_IOT_REGION = "us-west-2";
+    final static String AWS_IOT_REGION = "us-east-1";
 }
